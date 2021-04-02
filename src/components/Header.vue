45e3e3461c95e90e08e188a62d2047bbd911e3f6
Carlos Ruiz <car.ruiz90@gmail.com>
2021-04-02 13:51:06 -0600

    Adding logos and defining navigation items

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav>
+      <img
+        class="nav-item"
+        alt="carlos logo"
+        width="100px"
+        src="@/assets/cruiz_logo_small.png"
+      />
+      <a
+        class="nav-item"
+        v-for="menu in menus"
+        :key="menu.name"
+        href="menu.url"
+        >{{ menu.name }}</a
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data: () => ({
+    menus: [
+      {name: 'About me', url: '/about-me'},
+      {name: 'Curriculum', url: '/curriculum'},
+      {name: 'Projects', url: '/projects'}
+    ]
+  })
+}
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  display: flex;
+  padding: 2em;
+  align-items: center;
+}
+
+.nav-item {
+  margin: 0 2em;
+}
+</style>
